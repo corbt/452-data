752df441a91d9d7044c64d9efc62f9f6993ad74f
Kyle Corbitt <kyle@corbt.com>
2013-09-24 09:58:18 -0600

    Project 1 files added

---
+++ proj1_code_example.sql
@@ -0,0 +1,9 @@
+-- Recommended way to start for Postgres
+
+-- To run this file from command line type "psql -f proj1_code_example.sql"
+-- To run in Sublime install the included SQL.sublime-build, restart Sublime and hit "CTRL+B".
+
+-- \c p1 -- Connects to the database "p1"
+\i proj1.sql -- Will run the code in proj1.sql to reset the database before each query
+
+SELECT * FROM Product; -- Type your query here
